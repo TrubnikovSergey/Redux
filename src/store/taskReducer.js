64614783e3ab436b7e46288b84bc4e59e967eea8
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-10-19 18:46:42 +0300

    finished home work lesson18

--- src/store/taskReducer.js
+++ src/store/taskReducer.js
@@ -1,19 +1,26 @@
+import { taskDeleted } from "./actionTypes";
 import { taskUpdated } from "./actionTypes";
 
 export function taskReducer(state = [], action) {
-    switch (action.type) {
-        case taskUpdated: {
-            const newArray = [...state];
-            const elementIndex = newArray.findIndex(
-                (el) => el.id === action.payload.id
-            );
-            newArray[elementIndex] = {
-                ...newArray[elementIndex],
-                ...action.payload,
-            };
-            return newArray;
-        }
-        default:
-            return state;
+  switch (action.type) {
+    case taskUpdated: {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      );
+      newArray[elementIndex] = {
+        ...newArray[elementIndex],
+        ...action.payload,
+      };
+      return newArray;
     }
+    case taskDeleted: {
+      console.log(state);
+      const newArray = state.filter((item) => item.id !== action.payload.id);
+      console.log(newArray);
+      return newArray;
+    }
+    default:
+      return state;
+  }
 }
