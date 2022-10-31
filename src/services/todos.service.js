5890fd4d12c63541d8b190e21a65e5470c69e5df
Trubnikov Sergey <trubnikovsergey1@gmail.com>
2022-10-31 19:48:17 +0300

    home work finished

--- src/services/todos.service.js
+++ src/services/todos.service.js
@@ -1,14 +1,19 @@
 import httpService from "./http.service";
 const todosEndepoint = "todos/";
 const todosService = {
-    fetch: async () => {
-        const { data } = await httpService.get(todosEndepoint, {
-            params: {
-                _page: 1,
-                _limit: 10,
-            },
-        });
-        return data;
-    },
+  fetch: async () => {
+    const { data } = await httpService.get(todosEndepoint, {
+      params: {
+        _page: 1,
+        _limit: 10,
+      },
+    });
+    return data;
+  },
+
+  create: async (todo) => {
+    const { data } = await httpService.post(todosEndepoint, todo);
+    return data;
+  },
 };
 export default todosService;
